17ea55afba7a96b035a441358d23e9ab718f0d8c
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-09-29 09:29:40 +0200

    added bootstrap & redux dependencies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={'https://media.giphy.com/media/xT9KVqOt8xuRYhNpq8/giphy.gif'} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Phonebook</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+          <Button color="danger">Danger!</Button>
       </div>
     );
   }
