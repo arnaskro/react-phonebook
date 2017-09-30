e1cb4d7c9d0f1de32655c386d17a451e42da4f0c
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-09-30 19:45:06 +0200

    Contacts components
    For displaying the Contacts List and input fields

---
+++ src/components/Contacts.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import * as actions from '../actions/ContactsActions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Row, Col, Input, InputGroup, InputGroupButton, Button } from 'reactstrap';
+
+
+import ContactsList from './ContactsList';
+
+class Contacts extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <br />
+        <Row>
+          <Col xs="12">
+            <InputGroup>
+              <Input 
+                ref="name" 
+                type="text" 
+                placeholder="Name" 
+                value={this.props.input.name}
+                onChange={(e) => this.props.actions.inputName(e.target.value)}/>
+              <Input 
+                type="text"
+                placeholder="Phonenumber"
+                value={this.props.input.phonenumber}
+                onChange={(e) => this.props.actions.inputPhonenumber(e.target.value)}/>
+              <InputGroupButton>
+                <Button 
+                  disabled={this.props.input.name.length < 2 || this.props.input.phonenumber.length < 2} 
+                  color="success" 
+                  onClick={() => {
+                   this.props.actions.add(this.props.data.length, this.props.input.name, this.props.input.phonenumber);    
+                  }}>Add</Button>
+              </InputGroupButton>
+            </InputGroup>
+          </Col>
+          <Col xs="12">
+            <ContactsList data={this.props.data} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+
+const mapStateToProps = (state) => {
+	return {
+    data: state.contacts.data,
+    input: state.contacts.input
+	};
+};
+
+const mapDisptachToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+};
+
+export default connect(mapStateToProps, mapDisptachToProps)(Contacts);
