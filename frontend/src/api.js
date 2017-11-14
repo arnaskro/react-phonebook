f20505b6d23ec7fb7c907777fff1b800415a89d3
Arnas Kromelis <arnas.kromelis@gmail.com>
2017-11-14 20:50:32 +0100

    delete action

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,3 +2,4 @@ const BASE_URL = 'http://localhost:8080';
 
 export const GetContactsRequest = () => BASE_URL + '/contacts';
 export const DeleteContactReqest = () => BASE_URL + '/contact';
+export const CreateContactReqest = () => BASE_URL + '/contact';
